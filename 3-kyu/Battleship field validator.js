c64c538ca310c2be3def8c335e5e486e23df438b
ItsRazyHero <58303566+ItsRazyHero@users.noreply.github.com>
2021-11-19 17:19:25 +0300

    Update Battleship field validator.js
    Without comments

--- 3-kyu/Battleship field validator.js
+++ 3-kyu/Battleship field validator.js
@@ -19,9 +19,9 @@ function validateBattlefield(field) {
   for (var ships = [10,0,0,0,0], row = 0; row < 10; row++) {
     for (var col = 0; col < 10; col++) {
       if ( hit(row,col) ) {
-        if ( hit(row-1, col-1) || hit(row-1, col+1) ) return false; // Corner is touching
-        if ( hit(row-1, col  ) && hit(row  , col-1) ) return false; // Side is touching
-        if ( ( field[row][col] += hit(row-1, col) + hit(row, col-1) ) > 4 ) return false; // Ship is too long
+        if ( hit(row-1, col-1) || hit(row-1, col+1) ) return false;
+        if ( hit(row-1, col  ) && hit(row  , col-1) ) return false;
+        if ( ( field[row][col] += hit(row-1, col) + hit(row, col-1) ) > 4 ) return false;
         ships[field[row][col]]++; ships[field[row][col] - 1]--;
   } } }
   return [0,4,3,2,1].every((s,i) => s == ships[i]);
